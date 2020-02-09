084629962c10cf2e62ed9c9849edfe1cd29b823a
RacoonWally <eeewwwrrr@mail.ru>
2020-02-10 01:28:09 +0300

    анимация плюс корректировки

--- src/scripts/first-slide.js
+++ src/scripts/first-slide.js
@@ -1,1 +1,11 @@
+const smallDot = document.querySelector('.small_dot');
+const bigDots = document.querySelectorAll('.big_dot');
 
+// for (i = 0; i < smallDots.length; i++){
+//     smallDots[i].animate([
+//         {width: 0},{width: 100}
+//     ], {duration: 1000, iterations: Infinity})
+// }
+
+smallDot.animate([ {pulse(value, duration) {1 + 's', ease
+    }},{width: 100}], {duration: 1000, iterations: Infinity});
